bfb4e66d89666902fe72015cc5ce206801ba35eb
Aussenstelle <70533695+Aussenstelle@users.noreply.github.com>
2021-10-26 16:08:54 +0200

    Add files via upload

---
+++ fontinstaller.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+print('FontLoader initalisiert...')
+if sys.platform == 'linux' or platform == 'linux2':
+    print('Linux erkannt.')
+else:
+    if sys.platform == 'darwin':
+        betriebssystem = 'Mac OS'
+    if sys.platform == 'win32':
+        betriebssystem = 'Windows'
+    print('Dieser installer funktioniert nur auf Ubuntu. Auf ' + betriebssystem + 'Funktioniert dieses Programm nicht. Exiting...')
+    sys.exit(0)
+# Standart Benutzer
+benutzer = 'administrator'
+# Gebe den Pfad wohin die Schriften kopiert werden sollen
+Schriften_pfad = '/usr/share/fonts/truetype/'
+try:
+    d = open('r', 'config.txt')
+except:
+    print('Failed to read config.txt')
+    sys.exit(0)
+path = d.readline()
+while True:
+        link = d.readline()
+        if link == '':
+            break
+        befehl = 'wget -O' + path + ' ' + link
+        os.system(befehl)
+        os.system('unzip ' + path)
+        os.system('cp /home/administrator/schriften/fuellerschrift/BlackSignature_PERSONAL_USE_ONLY.otf /usr/share/fonts/truetype/')
